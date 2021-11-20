172a518f0a845484d944e5d2f6de42db8a92690d
Grid112 <sumitware1999@gmail.com>
2021-11-21 01:15:43 +0530

    ADDED: Establishing Database Connection

--- src/main/java/com/bridgelabz/jdbc/DatabaseConnection.java
+++ src/main/java/com/bridgelabz/jdbc/DatabaseConnection.java
@@ -1,7 +1,19 @@
 package com.bridgelabz.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
-    public static void main(String[] args) {
-        System.out.println("Welcome To JDBC Connectivity program");
+
+    public Connection getConnection() throws SQLException {
+        String jdbcURL = "jdbc:mysql://localhost:3306/address_book_service?useSSL=false";
+        String userName = "root";
+        String password = "Sumit123@Sumit@123";
+        Connection connection;
+        System.out.println("Connecting to database: "+jdbcURL);
+        connection = DriverManager.getConnection(jdbcURL, userName, password);
+        System.out.println("Connection is successful! " +connection);
+        return connection;
     }
 }
